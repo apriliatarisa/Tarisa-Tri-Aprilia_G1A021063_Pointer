79b9ebe04a73501cf85c5417b0743231ab539f0c
tarisaSDA <99057720+tarisaSDA@users.noreply.github.com>
2022-02-27 20:15:40 +0700

    Add files via upload

---
+++ pointer2,tarisa,063.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+ int a,b,c;
+ int *P1,*P2,*P3;
+ a=10;
+ cout<<a<<"\n";
+ b=15;
+ cout<<b<<"\n";
+ c=27;
+ cout<<c<<"\n";
+ P1=&a;
+ cout<<P2<<"\n";
+ P2=&b;
+ cout<<&b<<"\n";
+ P1=&c;
+ cout<<P1<<"\n";
+ a=*P2;
+ cout<<a<<"\n";
+ b=6;
+ cout<<b<<"\n";
+ P3=&b;
+ cout<<P3<<"\n";
+ P3=&c;
+ cout<<P3<<"\n";
+ *P1=*P3;
+ cout<<*P3<<"\n";
+}
